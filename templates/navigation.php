2b1d9c96deaf0968357ab7fcb40ffe6e76f467b0
markfullmer <mfullmer@gmail.com>
2017-09-30 09:21:50 -0500

    Add navigation tag

--- templates/navigation.php
+++ templates/navigation.php
@@ -6,5 +6,6 @@
       <li><a href="<?php echo $base_url;?>about">About</a></li>
       <li><a href="<?php echo $base_url;?>participate">Participate</a></li>
     </ul>
+    <span class="tag">Keep the plot. Keep the prose. Change the pronouns.</span>
   </div>
 </nav>
