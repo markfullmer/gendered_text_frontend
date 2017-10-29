91bf595a8a8835b8e4bd53ed2ed1be64e40753f5
markfullmer <mfullmer@gmail.com>
2017-10-29 14:07:48 -0500

    Add sort by genre & sort by word count

--- dynamic/text_list.php
+++ dynamic/text_list.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @file
+ * Lists available texts.
+ */
+
 use GuzzleHttp\Client;
 
 $texts = [];
@@ -16,19 +21,80 @@ $texts = (array) json_decode(file_get_contents('cache/texts.json'));
 $allowed = (array) json_decode(file_get_contents('allowed.json'));
 
 if ($texts) {
-  foreach ($texts as $id => $title) {
+  $rows = [];
+  $genres = [];
+  foreach ($texts as $id => $values) {
     if (in_array($id, $allowed)) {
-      $links[] = '<a href="/selection/' . $id . '">' . $title . '</a>';
+      // This first if statement is only to support legacy
+      // text listing. It can be removed after verifying update.
+      if (is_string($values)) {
+        $rows[$id]['title'] = '<a href="/selection/' . $id . '">' . $values . '</a>';
+        $rows[$id]['genre'] = '';
+        $rows[$id]['wordcount'] = '';
+      }
+      else {
+        $rows[$id]['title'] = $values->title;
+        $rows[$id]['link'] = '<a href="/selection/' . $id . '">' . $values->title . '</a>';
+        $rows[$id]['genre'] = $values->genre;
+        $rows[$id]['wordcount'] = $values->wordcount;
+        $genrekey = strtolower(str_replace(' ', '-', $values->genre));
+        $genres[$genrekey] = $values->genre;
+      }
     }
   }
 }
 
-if ($links) {
+if (!empty($rows)) {
   echo '<h3>Available Texts</h3>';
+  // Genre selection.
+  if (!empty($genres)) {
+    echo '<form action="/text_list" method="get">';
+    echo '<label for="bygenre">View by genre</label>';
+    echo '<select name="bygenre">';
+    foreach ($genres as $key => $genre) {
+      echo '<option value="' . $key . '"';
+      if (isset($_GET['bygenre'])) {
+        if ($_GET['bygenre'] == $key) {
+          echo ' selected="selected"';
+        }
+      }
+      echo '>' . $genre . '</option>';
+    }
+    echo '</select>';
+    echo '<input type="submit" name="submit" value="Filter" />';
+    echo '</form>';
+  }
+  if (isset($_GET['bygenre']) && in_array($_GET['bygenre'], array_keys($genres))) {
+    foreach ($rows as $id => $row) {
+      $thisgenre = $_GET['bygenre'];
+      if ($row['genre'] != $genres[$thisgenre]) {
+        unset($rows[$id]);
+      }
+    }
+  }
+  // Sort functionality:
+  if (isset($_GET['sort'])) {
+    if ($_GET['sort'] == 'title') {
+      usort($rows, function ($a, $b) {
+        return strcmp($a["title"], $b["title"]);
+      });
+    }
+    if ($_GET['sort'] == 'genre') {
+      usort($rows, function ($a, $b) {
+        return strcmp($a["genre"], $b["genre"]);
+      });
+    }
+    if ($_GET['sort'] == 'words') {
+      usort($rows, function ($a, $b) {
+          return $a['wordcount'] - $b['wordcount'];
+      });
+    }
+  }
+  $queries = isset($_GET['bygenre']) ? '&bygenre=' . $_GET['bygenre'] : '';
   echo '<table>';
-  echo '<thead><th>Title</th></thead>';
-  foreach ($links as $link) {
-    echo '<tr><td>' . $link . '</td></tr>';
+  echo '<thead><th><a href="/text_list?sort=title' . $queries . '">Title</a></th><th><a href="/text_list?sort=genre' . $queries . '">Genre</a></th><th><a href="/text_list?sort=words' . $queries . '">Word count</a></th></thead>';
+  foreach ($rows as $id => $row) {
+    echo '<tr><td>' . $row['link'] . '</td><td>' . $row['genre'] . '</td><td>' . number_format($row['wordcount']) . '</td></tr>';
   }
   echo '</table>';
 }
